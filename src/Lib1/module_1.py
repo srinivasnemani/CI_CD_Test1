2d4975512bcf2d4baeeef372a8f1fe206f699918
Srinivas Nemani <mail2srinivasnemani@gmail.com>
2023-12-02 10:43:02 +0000

    First commit in CI_CD_Test

---
+++ src/Lib1/module_1.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+    return a + b
+
+def substraction(a, b):
+    return a-b
+
+if __name__ == "__main__":
+    print("hello")
+    print("-----------------")
+    print(addition(10, 22))
+    print("-----------------")
---
+++ test/test_module1.py
@@ -0,0 +1,8 @@
+import pytest
+from ..src.Lib1.module_1 import addition  
+
+def test_addition():
+    assert addition(2, 3) == 5
+    assert addition(-1, 1) == 0
+    assert addition(0, 0) == 0
+    assert addition(100, -50) == 50
